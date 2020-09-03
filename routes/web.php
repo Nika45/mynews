1f24f39cba5a10cda6605c892c710ec855f1713d
Nika <kameeri@icloud.com>
2020-09-03 14:13:09 +0000

    kadai5

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Route::get('login', 'AAAContoroller@bbb');
+
+
